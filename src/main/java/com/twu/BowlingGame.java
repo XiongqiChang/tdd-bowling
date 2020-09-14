508018422cee1cd4e31967072348573a2648cdee
XiongqiChang <2838159948@qq.com>
2020-09-14 12:26:56 +0800

    完成简单的计分功能

---
+++ src/main/java/com/twu/BowlingGame.java
@@ -0,0 +1,20 @@
+package com.twu;
+
+/**
+ * @Author: xqc
+ * @Date: 2020/9/14 - 09 - 14 - 12:10
+ * @Description: com.twu
+ * @version: 1.0
+ */
+public class BowlingGame {
+    private int[] rolls = new int[10];
+    private int index = 0;
+    public void roll(int pins){
+        rolls[index++]=pins;
+    }
+
+    public int score() {
+
+        return  0;
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,22 @@
+import com.twu.BowlingGame;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class BowlingGameTest {
+
+
+    @Test
+    void shot_zero_number(){
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 10; i ++){
+            game.roll(i);
+        }
+        assertEquals(0,game.score());
+    }
+
+
+
+
 }
