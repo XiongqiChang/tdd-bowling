64448d12a8893b2fee5dcb4397730927b3d4313a
XiongqiChang <2838159948@qq.com>
2020-09-14 12:42:47 +0800

    修复下标的值

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -10,6 +10,16 @@ public class BowlingGameTest {
     @Test
     void shot_zero_number(){
         BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 20; i ++){
+            game.roll(i);
+        }
+        assertEquals(0,game.score());
+    }
+
+
+    @Test
+    void shot_score_number(){
+        BowlingGame game = new BowlingGame();
         for (int i = 0; i < 10; i ++){
             game.roll(i);
         }
